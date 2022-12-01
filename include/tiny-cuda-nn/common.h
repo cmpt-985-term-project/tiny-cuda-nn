c0b56d1f33dbd943e3fd1e0adf4a9a228742d98a
Geoffrey Mantel <gmantel@sfu.ca>
2022-12-01 14:45:25 -0800

    Recompiling to use FP32 by default for Python bindings. See Issue#51

--- include/tiny-cuda-nn/common.h
+++ include/tiny-cuda-nn/common.h
@@ -51,7 +51,9 @@ TCNN_NAMESPACE_BEGIN
 
 static constexpr uint32_t MIN_GPU_ARCH = TCNN_MIN_GPU_ARCH;
 
-#define TCNN_HALF_PRECISION (!(TCNN_MIN_GPU_ARCH == 61 || TCNN_MIN_GPU_ARCH <= 52))
+// Recompiling to use FP32 params by default
+// See: https://github.com/NVlabs/tiny-cuda-nn/issues/51
+#define TCNN_HALF_PRECISION 0 //(!(TCNN_MIN_GPU_ARCH == 61 || TCNN_MIN_GPU_ARCH <= 52))
 
 // TCNN has the following behavior depending on GPU arch.
 // Refer to the first row of the table at the following URL for information about
